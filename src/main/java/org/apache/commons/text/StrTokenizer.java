6fe5b4c6401dc5f770a813606be56e2ef0b192cc
7admiral23 <goldennumber2019@gmail.com>
2024-01-15 22:44:31 +0100

    reset non null

--- src/main/java/org/apache/commons/text/StrTokenizer.java
+++ src/main/java/org/apache/commons/text/StrTokenizer.java
@@ -450,18 +450,9 @@ public class StrTokenizer implements ListIterator<String>, Cloneable {
         try {
             return cloneReset();
         } catch (final CloneNotSupportedException ex) {
-            return createNonNullClone();
+            return null;
         }
     }
-    
-    private Object createNonNullClone() {
-        try {
-            return super.clone();
-        } catch (CloneNotSupportedException e) {
-            return new Object();
-        }
-    }
-    
 
     /**
      * Creates a new instance of this Tokenizer. The new instance is reset so that
